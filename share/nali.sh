a7879ff5182bf7eef7325ae7443ad494ba2d43e1
liqsliu <liqsliu@gmail.com>
2023-08-31 13:20:18 +0800

    Update nali.sh

--- share/nali.sh
+++ share/nali.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if test $# -gt 0
 then
-    echo $@|perl /usr/local/share/nali.pl
+    echo $@|perl /usr/local/share/nali/nali.pl
 else
-    perl /usr/local/share/nali.pl
+    perl /usr/local/share//nali/nali.pl
 fi
